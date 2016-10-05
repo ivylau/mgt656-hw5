2be6b7319dd35f81b6a7bf09187b91e9dbaa3ed1
Ivy Lau <ivy.lau@yale.edu>
2016-10-05 19:36:26 +0000

    add nickname route

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ app.get('/', function(request, response){
     response.send('Hello MGT-656!');
 });
 
+app.get('/alert-dolphin', function(request, response){
+    response.send('Hello MGT-656! How are you?');
+});
+
 app.listen(process.env.PORT || 4000);
